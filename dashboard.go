894a2b4690ba09b1282fda9546326518c8aefc72
Justin Dorfman <justindorfman@gmail.com>
2015-06-26 09:49:58 -0700

    for dima

--- dashboard.go
+++ dashboard.go
@@ -98,8 +98,8 @@ func main() {
 
 	bc := ui.NewBarChart()
 	bcdata := []int{3, 2, 5, 3, 9, 5, 3, 2, 5, 8, 3, 2, 4, 5, 3, 2, 5, 7, 5, 3, 2, 6, 7, 4, 6, 3, 6, 7, 8, 3, 6, 4, 5, 3, 2, 4, 6, 4, 8, 5, 9, 4, 3, 6, 5, 3, 6}
-	bclabels := []string{"S0", "S1", "S2", "S3", "S4", "S5"}
-	bc.Border.Label = "Bar Chart"
+	bclabels := []string{"200", "206", "304", "403", "400", "500", "499"}
+	bc.Border.Label = " Status Codes "
 	bc.Width = 26
 	bc.Height = 10
 	bc.X = 51
